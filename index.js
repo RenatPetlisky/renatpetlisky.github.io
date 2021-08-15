67499ec607211d1cc292a7bbd33714df2629b250
RenatPetlisky <86952252+RenatPetlisky@users.noreply.github.com>
2021-08-15 21:10:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ fetch('https://ipapi.co/json/')
   .then(d => d.json())
   .then(d => document.querySelector('#ip').innerHTML = d.ip);
   
-fetch('https://ip-api.com/json')
+fetch('http://ip-api.com/json')
   .then(d => d.json())
   .then(d => document.querySelector('#ipinfo').innerHTML = "Страна/Регион:<br>" + d.country + ", " + d.regionName + "<br><br>Провайдер:<br>" + d.org);
