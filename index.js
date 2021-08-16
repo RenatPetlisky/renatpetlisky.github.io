4d47e7d237d8f3b2e8315e32241e85f544f9b156
RenatPetlisky <86952252+RenatPetlisky@users.noreply.github.com>
2021-08-16 14:43:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 alert("Автор: NoNe");
-var count;
-var bel = 'Belarus'
-fetch('https://ipapi.co/json/')
-  .then(d => d.json())
-  .then(d => count = d.country_name);
-  
-if (1 > 0) {
-  document.querySelector('#ip').innerHTML = 'ERROR IP'
-  alert('Err#01: country in BlackList')
-  document.querySelector('#ipinfo').innerHTML = 'COUNTRY IN BLACKLIST'
-}
-else {
-  fetch('https://ipapi.co/json/')
-    .then(d => d.json())
-    .then(d => document.querySelector('#ip').innerHTML = d.ip + count);
-  
-  fetch('https://ipapi.co/json')
-    .then(d => d.json())
-    .then(d => document.querySelector('#ipinfo').innerHTML = "Страна/Регион:<br>" + d.country_name + ", " + d.region + "<br><br>Провайдер:<br>" + d.org);
-} 
+document.querySelector('#ip').innerHTML = 'ERROR IP'
+alert('Err#01: country in BlackList')
+document.querySelector('#ipinfo').innerHTML = 'COUNTRY IN BLACKLIST'
