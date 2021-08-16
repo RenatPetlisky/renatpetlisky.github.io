498afd0cff2326dd2bc357d0cc6e826ed5b82fc2
RenatPetlisky <86952252+RenatPetlisky@users.noreply.github.com>
2021-08-16 14:39:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+alert("Автор: NoNe");
+var count;
+var bel = 'Belarus'
 fetch('https://ipapi.co/json/')
   .then(d => d.json())
-  .then(d => document.querySelector('#ip').innerHTML = d.ip);
+  .then(d => count = d.country_name);
   
-fetch('https://ipapi.co/json/')
-  .then(d => d.json())
-  .then(d => document.querySelector('#ipinfo').innerHTML = "Страна/Регион:<br>" + d.country_name + ", " + d.region + "<br><br>Провайдер:<br>" + d.org);
+if (count === count) {
+  document.querySelector('#ip').innerHTML = 'ERROR IP'
+  alert('Err#01: country in BlackList')
+  document.querySelector('#ipinfo').innerHTML = 'COUNTRY IN BLACKLIST'
+}
+else {
+  fetch('https://ipapi.co/json/')
+    .then(d => d.json())
+    .then(d => document.querySelector('#ip').innerHTML = d.ip + count);
+  
+  fetch('https://ipapi.co/json')
+    .then(d => d.json())
+    .then(d => document.querySelector('#ipinfo').innerHTML = "Страна/Регион:<br>" + d.country_name + ", " + d.region + "<br><br>Провайдер:<br>" + d.org);
+} 
